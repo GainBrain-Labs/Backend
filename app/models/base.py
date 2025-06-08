d5197292c430b84bdaddaebb89298bbd762a3fee
Nishant <crcnish0710@gmail.com>
2025-06-08 16:27:47 +0530

    sql alchemy initialised with metadata class

--- app/models/base.py
+++ app/models/base.py
@@ -1,11 +1,10 @@
 from datetime import datetime
-from sqlalchemy.ext.declarative import declarative_base
+from db.base import Base
 from sqlalchemy import Column, Integer, DateTime
-
-Base = declarative_base()
 
 class Metadata(Base):
     __tablename__ = 'metadata'
     id = Column(Integer, primary_key=True)
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now) 
+
