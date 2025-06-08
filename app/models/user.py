827a88c1abe68e583bcd54970ad4e69b7c3b397b
Nishant <crcnish0710@gmail.com>
2025-06-08 18:54:45 +0530

    adding user

---
+++ app/models/user.py
@@ -0,0 +1,15 @@
+from models.base import BasicModel
+from sqlalchemy import Column, String,Date,Boolean
+
+class User(BasicModel):
+    __tablename__ = "user"
+
+    username = Column(String,unique=True, nullable=False)
+    first_name = Column(String,nullable=False)
+    last_name = Column(String)
+    password = Column(String,nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    profile_pic = Column(String, nullable=True)
+    phone_number = Column(String, unique=True, nullable=True)
+    dob = Column(Date, nullable=True)
+    is_active = Column(Boolean, default=True)
