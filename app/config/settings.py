aa2433f897b4d20ef76107e88c3013755321ca12
nityanandkumar <kumar610nityanand@gmail.com>
2025-06-08 19:45:49 +0530

    edited name for env variables

--- app/config/settings.py
+++ app/config/settings.py
@@ -4,15 +4,9 @@ from pydantic_settings import BaseSettings
 class DatabaseSettings(BaseSettings):
   db_username: str
   db_password: SecretStr
-  ip_address: str
-  port: int
-  database_name: str
+  db_host_address: str
+  db_port: int
+  db_database_name: str
   class Config:
     env_file = ".env"
     env_file_encoding = "utf-8"
-
-# if __name__ == "__main__":
-#     settings = DatabaseSettings()
-
-#     settings.model_dump()
-    #print(f"postgress://{settings.db_username}:{settings.db_password.get_secret_value()}@{settings.ip_address}:{settings.port}/{settings.database_name}")
