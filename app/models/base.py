f6c978b82a7489e36c737987dffc3d25aedcf218
Nishant <crcnish0710@gmail.com>
2025-06-08 16:32:58 +0530

    removing extra line

--- app/models/base.py
+++ app/models/base.py
@@ -7,4 +7,3 @@ class Metadata(Base):
     id = Column(Integer, primary_key=True)
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now) 
-
