ca7745c1f5539af2ad9b90dd7b4ae8c4b3160014
Nishant <crcnish0710@gmail.com>
2025-06-08 16:59:06 +0530

    correct basic model

--- app/models/base.py
+++ app/models/base.py
@@ -2,8 +2,10 @@ from datetime import datetime
 from db.base import Base
 from sqlalchemy import Column, Integer, DateTime
 
-class Metadata(Base):
-    __tablename__ = 'metadata'
+class BasicModel(Base):
+    
+    __abstract__ = True
+
     id = Column(Integer, primary_key=True)
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now) 
