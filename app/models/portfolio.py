4e53e3274f68c9cbc00dabb1bd1878aa2581fc68
nityanandkumar <kumar610nityanand@gmail.com>
2025-06-09 01:23:56 +0530

    fixed bugs and import issue

--- app/models/portfolio.py
+++ app/models/portfolio.py
@@ -1,9 +1,10 @@
-from base import BasicModel
-from sqlalchemy import Column, String, Integer, ARRAY, ForeignKey, Mapped, mapped_column
+from app.models.base import BasicModel
+from sqlalchemy import Column, String, Integer, ARRAY, ForeignKey
+from sqlalchemy.orm import relationship
 
 class portfolio(BasicModel):
     __tablename__ = 'portfolio'
     
     name = Column(String(100), nullable=False, unique=True)
     stock_group_ids = Column(ARRAY(Integer), nullable=False)
-    user_id = Mapped[int] = mapped_column(Integer, ForeignKey(name="user.id"), nullable=False, index=True) 
+    user_id = relationship("User", foreign_keys='User.id',cascade="all, delete-orphan")
--- app/models/stock.py
+++ app/models/stock.py
@@ -1,7 +1,6 @@
-from base import BasicModel
-from sqlalchemy import Column, String, ARRAY, Integer
-from enums import stock_group_type
-from enum import Enum
+from app.models.base import BasicModel
+from sqlalchemy import Column, String, ARRAY, Integer, Enum
+from app.models.enums import stock_group_type
 
 class stock(BasicModel):
     __tablename__ = 'stock'
@@ -13,7 +12,7 @@ class stock_group(BasicModel):
     __tablename__ = 'stock_group'
     name = Column(String(100), nullable=False, unique=True)
     ticker = Column(String(20), nullable=False, unique=True, index=True)
-    stock_group_type =  Column(Enum(stock_group_type), nullable=False, nullable=False, unique=True)
+    stock_group_type =  Column(Enum(stock_group_type), nullable=False, unique=True)
     stock_ids = Column(ARRAY(Integer), nullable=False)
 
 
