6357e9512344fc0595df2df6286b6868434812a8
nityanandkumar <kumar610nityanand@gmail.com>
2025-06-09 16:36:35 +0530

    edited custom group model

--- app/models/portfolio.py
+++ app/models/portfolio.py
@@ -1,10 +1,9 @@
 from app.models.base import BasicModel
-from sqlalchemy import Column, String, Integer, ARRAY, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Integer, ARRAY, Integer, ForeignKey
 
 class portfolio(BasicModel):
     __tablename__ = 'portfolio'
     
     name = Column(String(100), nullable=False, unique=True)
     stock_group_ids = Column(ARRAY(Integer), nullable=False)
-    user_id = relationship("User", foreign_keys='User.id',cascade="all, delete-orphan")
+    user_id = Column(Integer, ForeignKey('user.id'),nullable=False)
--- app/models/stock.py
+++ app/models/stock.py
@@ -1,5 +1,5 @@
 from app.models.base import BasicModel
-from sqlalchemy import Column, String, ARRAY, Integer, Enum
+from sqlalchemy import Column, String, ARRAY, Integer, Enum, ForeignKey
 from app.models.enums import stock_group_type
 
 class stock(BasicModel):
@@ -7,12 +7,19 @@ class stock(BasicModel):
     name = Column(String(100),nullable=False, unique=True)
     ticker = Column(String(20), nullable=False, unique=True, index=True)
 
-
-class stock_group(BasicModel):
-    __tablename__ = 'stock_group'
+class stock_group_base:
     name = Column(String(100), nullable=False, unique=True)
     ticker = Column(String(20), nullable=False, unique=True, index=True)
-    stock_group_type =  Column(Enum(stock_group_type), nullable=False, unique=True)
+    stock_group_type =  Column(Enum(stock_group_type), nullable=False)
     stock_ids = Column(ARRAY(Integer), nullable=False)
+
+
+class stock_group(stock_group_base,BasicModel):
+    __tablename__ = 'stock_group'
+
+class custom_stock_group(stock_group_base,BasicModel):
+    __tablename__ = 'custom_stock_group'
+    user_id = Column(Integer,ForeignKey('user.id'), nullable=False)
+
 
 
