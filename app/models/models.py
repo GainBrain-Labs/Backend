e23380a9405af4c911714e9fda611a85db04ab6c
Nishant <crcnish0710@gmail.com>
2025-06-22 02:44:40 +0530

    migrations for initial files

--- app/models/models.py
+++ app/models/models.py
@@ -2,3 +2,6 @@ from app.models import Base
 from app.models.user import User
 from app.models.stock import Stock
 from app.models.portfolio import Portfolio
+from app.models.backtest import Backtest
+from app.models.compare import Compare
+from app.models.strategy import Strategy
